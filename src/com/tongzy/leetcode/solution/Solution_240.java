5094d8f0dc77161d0d43e6ff83c9c9b22cc55953
Michael <tzy_2008@yeah.net>
2019-12-04 21:50:35 +0800

    solution 1 for 240. Search a 2D Matrix II

--- src/com/tongzy/leetcode/solution/Solution_240.java
+++ src/com/tongzy/leetcode/solution/Solution_240.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 
 /**
  * 240. Search a 2D Matrix II
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * Example:
+ *
+ * Consider the following matrix:
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * Given target = 5, return true.
+ *
+ * Given target = 20, return false.
  */
 public class Solution_240 {
     public boolean searchMatrix(int[][] matrix, int target) {
@@ -26,12 +45,32 @@ public class Solution_240 {
         if (target == compare) {
             return true;
         } else if (target < compare) {
-            return binarySearch(matrix, target, mLeft, mMid, nLeft, nMid);
+            return binarySearch(matrix, target, mLeft, mMid - 1, nLeft, nMid - 1)
+                    || binarySearch(matrix, target, mMid, mRight, nLeft, nMid - 1)
+                    || binarySearch(matrix, target, mLeft, mMid - 1, nMid, nRight);
         } else {
             //target > compare
             return binarySearch(matrix, target, mMid + 1, mRight, nLeft, nMid)
                     || binarySearch(matrix, target, mMid + 1, mRight, nMid + 1, nRight)
                     || binarySearch(matrix, target, mLeft, mMid, nMid + 1, nRight);
+        }
+    }
+/*
+    Runtime: 19 ms, faster than 5.01% of Java online submissions for Search a 2D Matrix II.
+    Memory Usage: 42.7 MB, less than 100.00% of Java online submissions for Search a 2D Matrix II.
+*/
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+        int num = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = num++;
+            }
+        }
+        Solution_240 solution = new Solution_240();
+        for (int i = 0; i < 30; i++) {
+            System.out.println(i+"  "+solution.searchMatrix(matrix, i));
         }
     }
 }
