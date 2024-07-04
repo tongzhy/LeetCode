3dd061e0627f8c7961324940b83cc0b4943f4950
michael <tzy_2008@yeah.net>
2024-07-04 15:32:47 +0800

    S145_Binary_Tree_Postorder_Traversal

--- src/com/tongzy/leetcode/solution/S145_Binary_Tree_Postorder_Traversal.java
+++ src/com/tongzy/leetcode/solution/S145_Binary_Tree_Postorder_Traversal.java
@@ -31,4 +31,27 @@ public class S145_Binary_Tree_Postorder_Traversal {
         } while (!stack.isEmpty());
         return result;
     }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        traverseTree(root, result);
+        return result;
+    }
+
+    void traverseTree(TreeNode root, List<Integer> result) {
+        if (root.left == null && root.right == null) {
+            result.add(root.val);
+            return;
+        }
+        if (root.left != null) {
+            traverseTree(root.left, result);
+        }
+        if (root.right != null) {
+            traverseTree(root.right, result);
+        }
+        result.add(root.val);
+    }
 }
